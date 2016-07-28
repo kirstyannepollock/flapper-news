626362ba25fb6fb030783b9c9986671db5a7bee3
kirstyannepollock <kpollock@hotmail.co.uk>
2016-07-28 18:34:01 +0200

    methods added for creat and save but not working yet

--- app.js
+++ app.js
@@ -22,96 +22,106 @@ function postsController($scope, $stateParams, postFactory) {
   };
 }
 
-  function mainController($scope, postFactory) {
+function mainController($scope, postFactory) {
 
-    // holds all fns and vars that can be used in pages
-    $scope.app = {};
-    $scope.app.posts = postFactory.posts;
+  // holds all fns and vars that can be used in pages
+  $scope.app = {};
+  $scope.app.posts = postFactory.posts;
 
-    $scope.app.addPost = function (title, link, posts) {
-      addMockPost(title, link, posts);
+  $scope.app.addPost = function (title, link, posts) {
 
-      //persist changes back out
-      $scope.title = title;
-      $scope.link = link;
-    };
+    if (!title || title === '') { return; }
 
-    $scope.app.incrementUpvotes = incrementUpvotes;
-  }
+    posts.create({
+      title: title,
+      link: link,
+    });
 
-  function stateConfig($stateProvider, $urlRouterProvider) {
-
-    $stateProvider
-      .state('home',
-      {
-        // Ensure that anytime our home state is entered, we will automatically 
-        // query all posts from our backend before the state actually finishes loading.
-        resolve: {
-          postPromise: ['posts', function (posts) {
-            return posts.getAll();
-          }]
-        },
-        url: '/home',
-        templateUrl: '/home.html',
-        controller: 'MainController'
-      })
-      .state('posts',
-      {
-        url: '/posts/{id}',
-        templateUrl: '/posts.html',
-        controller: 'PostsController'
-      });
-
-    $urlRouterProvider.otherwise('home');
-  }
-
-  function postFactory($http) {
-    // What we're doing here is creating a new object that has an array
-    // property called 'posts'. We then return that variable so that our
-    // o object essentially becomes exposed to any other Angular module 
-    // that cares to inject it. You'll note that we could have simply 
-    // exported the posts array directly, however, by exporting an object
-    // that contains the posts array we can add new objects and methods 
-    // to our services in the future.
-    var o = { posts: [] };
-
-    // **TODO: work out how to inject this
-    var apiBaseUrl = 'http://localhost:3001';
-
-    o.getAll = function () 
-    {
-      return $http.get(apiBaseUrl + '/posts')
-        .success(function (data) 
-        {
-          angular.copy(data, o.posts);
-        });
-    };
-
-    return o;
+    $scope.title = '';
+    $scope.link = '';
   };
 
 
-  function addComment(post, body) {
-    if (body === '') { return; }
-    post.comments.push({
-      body: body,
-      author: 'user',
-      upvotes: 0
+  $scope.app.incrementUpvotes = incrementUpvotes;
+}
+
+function stateConfig($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home',
+    {
+      // Ensure that anytime our home state is entered, we will automatically 
+      // query all posts from our backend before the state actually finishes loading.
+      resolve: {
+        postPromise: ['posts', function (posts) {
+          return posts.getAll();
+        }]
+      },
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainController'
+    })
+    .state('posts',
+    {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsController'
     });
 
-    body = '';
-  }
+  $urlRouterProvider.otherwise('home');
+}
 
-  function incrementUpvotes(item) {
-    item.upvotes++;
-  }
+function postFactory($http) {
+  // What we're doing here is creating a new object that has an array
+  // property called 'posts'. We then return that variable so that our
+  // o object essentially becomes exposed to any other Angular module 
+  // that cares to inject it. You'll note that we could have simply 
+  // exported the posts array directly, however, by exporting an object
+  // that contains the posts array we can add new objects and methods 
+  // to our services in the future.
+  var o = { posts: [] };
 
-  function addMockPost(title, link, posts) {
-    if (title == "") { title = "default title"; };
-    var id = posts.length; //temps!!
-    posts.push({ id: id, title: title, link: link, upvotes: 0, comments: [] });
+  // **TODO: work out how to inject this
+  var apiBaseUrl = 'http://localhost:3001';
 
-    title = "";
-    link = "";
-  }
+  o.getAll = function () {
+    return $http.get(apiBaseUrl + '/posts')
+      .success(function (data) {
+        angular.copy(data, o.posts);
+      });
+  };
+
+  o.create = function (post) {
+    return $http.post(apiBaseUrl + '/posts', post).success(function (data) {
+      o.posts.push(data);
+    });
+  };
+
+  return o;
+};
+
+
+function addComment(post, body) {
+  if (body === '') { return; }
+  post.comments.push({
+    body: body,
+    author: 'user',
+    upvotes: 0
+  });
+
+  body = '';
+}
+
+function incrementUpvotes(item) {
+  item.upvotes++;
+}
+
+function addMockPost(title, link, posts) {
+  if (title == "") { title = "default title"; };
+  var id = posts.length; //temps!!
+  posts.push({ id: id, title: title, link: link, upvotes: 0, comments: [] });
+
+  title = "";
+  link = "";
+}
 
