ca85cdb7d19c6957b1addce175db3ca978af7989
kirstyannepollock <kpollock@hotmail.co.uk>
2016-07-28 12:35:00 +0200

    now with very pasic posts service (factory)

--- app.js
+++ app.js
@@ -2,21 +2,31 @@
 //https://thinkster.io/angular-rails#angular-services
 
 angular.module('flapperNews', [])
-  .controller('MainController', ['$scope', mainController]);
+  .factory('posts', postFactory)
+  .controller('MainController', ['$scope', 'posts', mainController]);
 
 
-function mainController($scope) {
+function mainController($scope, postFactory) {
 
   // holds all fns and vars that can be used in pages
   $scope.app = {};
 
+  $scope.app.posts = postFactory.posts; 
   $scope.app.posts = createMockPosts();
   $scope.app.addPost = addMockPost;
   $scope.app.incrementUpvotes = incrementUpvotes;
 }
 
-function factory() {
-
+function postFactory() {
+// What we're doing here is creating a new object that has an array
+// property called 'posts'. We then return that variable so that our
+// o object essentially becomes exposed to any other Angular module 
+// that cares to inject it. You'll note that we could have simply 
+// exported the posts array directly, however, by exporting an object
+// that contains the posts array we can add new objects and methods 
+// to our services in the future.
+  var o = { posts: [] };
+  return o;
 };
 
 function addPost(title) {
